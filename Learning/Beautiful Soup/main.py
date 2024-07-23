6f627530e690d763a58677e941425d7f80ebc197
Kanika Bhandari <145848814+KanikaBhandari24@users.noreply.github.com>
2024-07-23 20:44:47 +0530

    Add files via upload

--- Learning/Beautiful Soup/main.py
+++ Learning/Beautiful Soup/main.py
@@ -1,1 +1,10 @@
-
+from bs4 import BeautifulSoup #bs4 latest version of beautiful soup library
+#import lxml
+with open("/Users/Kanika Bhandari/OneDrive/Desktop/PYTHON/Web dev Projects in Python/5. Beautiful Soup Project/website.html", encoding="utf-8") as file:
+    contents=file.read()
+soup=BeautifulSoup(contents,"html.parser")
+print(soup.title) 
+print(soup.title.string)
+a=soup.find_all(name="a")
+for tag in a:
+    print(tag.getText()) 
